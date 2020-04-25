0233c77b48188ac45c44c69a712ef27c0b9e78ab
Kha Tran <1653032@student.hcmus.edu.vn>
2020-04-25 19:44:21 +0700

    Updates

---
+++ precache-manifest.627a7025643cabf6314e468ca295faa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f4ead5f983a6298833544b53b82726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76690fcb8de120389c73",
+    "url": "/static/css/2.2d487d2c.chunk.css"
+  },
+  {
+    "revision": "3d30582f3c99afe490a4",
+    "url": "/static/css/main.6b7a7e66.chunk.css"
+  },
+  {
+    "revision": "76690fcb8de120389c73",
+    "url": "/static/js/2.94fee9ff.chunk.js"
+  },
+  {
+    "revision": "92f320287b4a6131c85a8034a67626e7",
+    "url": "/static/js/2.94fee9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d30582f3c99afe490a4",
+    "url": "/static/js/main.61104607.chunk.js"
+  },
+  {
+    "revision": "be3e7db78920df238b05",
+    "url": "/static/js/runtime-main.32a91f49.js"
+  }
+]);
