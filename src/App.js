21111565576f9bf89647e5f1ef562fbdfd1bad76
Kha Tran <1653032@student.hcmus.edu.vn>
2020-05-01 20:17:39 +0700

    Added charts,router for midterm

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import moment from 'moment'
 import Dashboard from './components/dashboard';
+import Charts from './components/charts'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Row, NavLink, Col } from 'react-bootstrap';
 
 
 class App extends React.Component{
   render(){
     return (
-      <Dashboard></Dashboard>
+      <Router>
+        <nav class="navbar sticky-top navbar-dark bg-dark" style={{margin:'0 auto',display:'block',textAlign:'center',float:'none'}}>
+          <Row>
+            <Col lg={6}><Link to="/map" role="button" aria-pressed="true" className="navbar-brand">Dashboard</Link></Col>
+            <Col lg={6}><Link to="/stats" role="button" aria-pressed="true" className="navbar-brand">Charts</Link></Col>
+          </Row>
+        </nav>
+        <br/>
+        <br/>
+        <Switch>
+          <Route path="/map">
+          <Dashboard></Dashboard>
+          </Route>
+          <Route path="/stats">
+            <Charts></Charts>
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/charts.js
@@ -0,0 +1,195 @@
+import {defaults, Line} from 'react-chartjs-2'
+import React from 'react';
+import {Row,Col, Container} from 'react-bootstrap'
+
+class Charts extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            vnData: null,
+            vnLabels: null,
+            gbData: null,
+            gbLabels: null
+        }
+    }
+
+    componentDidMount() {
+
+        fetch("https://td.fpt.ai/corona/corona-chart-vn.json")
+        .then(res => res.json()).then(result=> {
+            let labels = Object.keys(result);
+            let Infected = labels.map(key => result[key][0]);
+            let Dead = labels.map(key => result[key][1]);
+            let Cured = labels.map(key => result[key][2]);
+            this.setState({
+                vnData: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Nhiễm',
+                        data: Infected,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(201, 38, 38,1)',
+                        borderColor: 'rgba(201, 38, 38,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                    },{
+                        label: 'Cách ly',
+                        data: Dead,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(75,192,192,0.4)',
+                        borderColor: 'rgba(75,192,192,0.4)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        
+                    },{
+                        label: 'Khỏi bệnh',
+                        data: Cured,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(38, 201, 38,1)',
+                        borderColor: 'rgba(38, 201, 38,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                    }]
+                }
+            });
+        });
+
+        fetch("https://td.fpt.ai/corona/corona-total.json")
+        .then(res => res.json()).then(result=> {
+            let labels = Object.keys(result);
+            let Infected = labels.map(key => result[key][0]);
+            let Dead = labels.map(key => result[key][1]);
+            let Cured = labels.map(key => result[key][2]);
+            this.setState({
+                gbData: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Infected',
+                        data: Infected,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(201, 38, 38,1)',
+                        borderColor: 'rgba(201, 38, 38,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                    },{
+                        label: 'Dead',
+                        data: Dead,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(75,192,192,0.4)',
+                        borderColor: 'rgba(75,192,192,0.4)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        
+                    },{
+                        label: 'Cured',
+                        data: Cured,
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: 'rgba(38, 201, 38,1)',
+                        borderColor: 'rgba(38, 201, 38,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                    }]
+                }
+            }
+            );
+        })
+    };
+
+    render(){
+        return(
+            <Container style={{maxWidth:'1500px'}}>
+                <div className="display-4 text-md-center">Corona Stats</div>
+              <div></div>
+            <Row>
+                <Col lg={6}>
+                <p class="text-center">Số ca mắc, cách ly và khỏi bệnh của Việt Nam</p>
+                    {this.state.vnData && <Line
+                        data={this.state.vnData}>
+                    </Line>}
+                </Col>
+                <Col lg={6}>
+                <p class="text-center">World's Corona Cases (Infected/Dead/Cured)</p>
+                    {this.state.vnData && <Line
+                        data={this.state.gbData}>
+                    </Line>}
+                </Col>
+            </Row>
+            </Container>
+        );
+    }
+}
+
+export default Charts
